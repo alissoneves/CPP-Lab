61a78a2d6f0decae10b82485afde5a8cf148adc4
alissoneves <alissoneves_it@outlook.com>
2026-03-15 16:32:43 -0300

    Teste após sucesso

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello Kubernetes CI/CD Pipeline!" << std::endl;
+    std::cout << "Hello Kubernetes CI/CD Pipeline v.2!" << std::endl;
     return 0;
 }
